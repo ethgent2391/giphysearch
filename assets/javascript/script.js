82eb4b2cd7ab663976d2499ac0f4e3a348a52ebb
Ethan Gent <ethan.gent.2391@gmail.com>
2018-12-03 22:24:45 -0800

    added dividers, dynamic id's

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,6 +7,7 @@ $("#search-exec").on("click", function(event) {
     const api_key = "&api_key=Tu55M0ctc39IXfgfMa78YUFLKqCrQzPH"
     const limit = "&limit=10"
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifsrch + api_key + limit;
+    var trimmed = gifsrch.replace(/ +/g, "");
 
     $.ajax({
         url: queryURL,
@@ -15,12 +16,14 @@ $("#search-exec").on("click", function(event) {
         buttons.push(gifsrch);
         console.log(buttons);
         console.log(response);
+        $("#results").prepend("<div id='"+trimmed+"'><h5>"+gifsrch+":</h5></div>")
+
           for(i=0; response.data.length; i++){
-            $("#results").prepend("<img class='m-1' src='"+response.data[i].images.fixed_height_small.url+"' alt='srchresult'>")
+            $("#"+trimmed).append("<img class='m-1' src='"+response.data[i].images.fixed_height_small.url+"' alt='srchresult'>")
           }
     })
 
-      $("#recent").append("<button type='button' class='btn button-info m-2'>"+gifsrch+"</button>");
+      $("#recent").append("<button id='"+trimmed+"btn' type='button' class='btn button-info m-2'>"+gifsrch+"</button>");
       $("#srchinput").val('');
 })
 
