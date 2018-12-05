7577a482d4ced452a39804fcbc291df6b60a9d06
Ethan Gent <ethan.gent.2391@gmail.com>
2018-12-04 18:06:21 -0800

    huzzah!

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -19,12 +19,25 @@ $("#search-exec").on("click", function(event) {
         $("#results").prepend("<div id='"+trimmed+"'><h5>"+gifsrch+":</h5></div>")
 
           for(i=0; response.data.length; i++){
-            $("#"+trimmed).append("<img class='m-1' src='"+response.data[i].images.fixed_height_small.url+"' alt='srchresult'>")
+            $("#"+trimmed).append("<img class='m-1 gif' src='"+response.data[i].images.fixed_height_still.url+"' data-still='"+response.data[i].images.fixed_height_still.url+ "' data-animate='"+response.data[i].images.fixed_height.url+"' data-state='still' alt='srchresult"+i+"'>")
           }
-    })
-
+          
+        });
+        
+        $(document).on("click",".gif", function() {
+            var state = $(this).attr("data-state"); 
+            if (state === "still"){
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+              };
+          })
       $("#recent").append("<button id='"+trimmed+"btn' type='button' class='btn button-info m-2'>"+gifsrch+"</button>");
+        
       $("#srchinput").val('');
+
 })
 
 };
